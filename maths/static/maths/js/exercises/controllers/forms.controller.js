8bc4a6aaf426138100672d37955151a425386f1c
Phong Nguyen <phongnt.ptnk@gmail.com>
2015-05-30 18:20:10 +0800

    working on style

--- maths/static/maths/js/exercises/controllers/forms.controller.js
+++ maths/static/maths/js/exercises/controllers/forms.controller.js
@@ -5,6 +5,7 @@
         .controller('FormController', ['$scope', '$http', function($scope, $http){
             $scope.formData = {};
             $scope.result = {};
+            $scope.user_answers = {};
             $scope.checkAnswer = function(question_id){
                 console.log("Checking!");
                 console.log(question_id);
@@ -23,13 +24,31 @@
                     console.log("error!");
                 })
             }
+            var selected_question, selected_answer, selected_index;
 
             $scope.latex_changed = function(){
                 console.log("changed");
                 var box = angular.element(document.getElementById("latex-box"));
-                $scope.ascii_input = box.mathquill('latex')
+                $scope.ascii_input = box.mathquill('latex');
                 console.log($scope.ascii_input);
+                if (!$scope.user_answers[selected_question]) {
+                    $scope.user_answers[selected_question] = {};
+                    console.log("defined "+selected_question);
+                }
+                if (!$scope.user_answers[selected_question][selected_answer]) {
+                    $scope.user_answers[selected_question][selected_answer] = {};
+                    console.log("defined "+selected_answer);
+                }
+                $scope.user_answers[selected_question][selected_answer][selected_index]=$scope.ascii_input;
             }
+            $scope.selected = function(question, answer, index) {
+                console.log(question, answer, index);
+                selected_question = question;
+                selected_answer = answer;
+                selected_index = index;
+                $scope.field
+            }
+
 
         }]);
 })()
