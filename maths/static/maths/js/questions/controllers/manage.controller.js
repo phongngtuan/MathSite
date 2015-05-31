de63b6cac75bc5caa60e67eccb17378d4f8ddc7e
Phong Nguyen <phongnt.ptnk@gmail.com>
2015-05-31 23:51:08 +0800

    load question upon selecting topic

--- maths/static/maths/js/questions/controllers/manage.controller.js
+++ maths/static/maths/js/questions/controllers/manage.controller.js
@@ -2,14 +2,24 @@
     'use strict';
     angular
         .module('maths.questions.controllers')
-        .controller('QuestionsManageController', ['$scope', 'Topic', 'Subject',
-                function($scope, Topic, Subject) {
+        .controller('QuestionsManageController', ['$scope', 'Topic', 'Subject', 'Question', 
+                function($scope, Topic, Subject, Question) {
                     Subject.query(function(response) {
                         $scope.subjects = response;
                     });
                     Topic.query(function(response) {
                         $scope.topics = response;
                     });
+                    $scope.topicChanged = function() {
+                        getQuestionWithTopic($scope.search.topic.id);
+                    }
+                    function getQuestionWithTopic(topic) {
+                        Question.getByTopic({topic: topic}, function(response) {
+                            $scope.questions = response; 
+                            console.log(response);
+                        })
+                        console.log($scope.search.topic.id);
+                    }
                 }
         ]);
 })();
