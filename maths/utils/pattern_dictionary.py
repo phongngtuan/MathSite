26d98e141524ada7a910ed1c0028ebef7338d577
Phong Nguyen <phongnt.ptnk@gmail.com>
2015-06-14 15:47:37 +0800

    add dictionary

--- maths/utils/pattern_dictionary.py
+++ maths/utils/pattern_dictionary.py
@@ -1,10 +1,12 @@
 rules = [
+    (r'\\left\(', r'('),
+    (r'\\right\)', r')'),
     (r'\\(sin|cos|tan|cot)\^([^\{\}\(\)]+)\s([^\{\}\(\)]+)',  r'\1(\3)**(\2)'),#trigonometric with power
     (r'\\(sin|cos|tan|cot)\s+([^\{\}\(\)]+)',  r'\1(\2)'),  #trigonometric
     (r'\\sqrt\{([^\{\}]+)\}', r'sqrt(\1)'), #square_root
     (r'([^\{\}]+)\^([^\{\}]+)', r'\1**\2'), # power
     (r'\\frac\{([^\{\}]+)\}\{([^\{\}]+)\}', r'(\1)/(\2)'), #fraction
     (r'\)\(',r')*('), #2 consecutive parentheses
-    (r'\)\s(\S+)',r')*\1'), #closing parentheses + symbols
+    (r'\)\s([\\\w]+)',r')*\1'), #closing parentheses + symbols
     (r'(\d)([A-Za-z])',r'\1*\2'),
     ]
